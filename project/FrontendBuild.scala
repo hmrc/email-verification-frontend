15123f8172a95abfc858ae76314b261d398c3eb2
Dominic Clifton <me@dominicclifton.name>
2017-04-13 14:04:33 +0100

    TSV-NIJ: Bump govuk-template to 5.2.0.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -15,7 +15,7 @@ private object AppDependencies {
   private val playHealthVersion = "2.1.0"
   private val logbackJsonLoggerVersion = "3.1.0"
   private val frontendBootstrapVersion = "7.22.0"
-  private val govukTemplateVersion = "5.1.0"
+  private val govukTemplateVersion = "5.2.0"
   private val playUiVersion = "7.0.0"
   private val playAuthorisedFrontendVersion = "6.3.0"
   private val playConfigVersion = "4.3.0"
