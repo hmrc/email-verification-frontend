e6f25faee9cdf184707beac3ac056e670338b637
Max Worgan <1410832+MaxWorgan@users.noreply.github.com>
2019-10-16 12:19:19 +0100

    AIV-1473 added welsh translation capabilities, currently disabled as no welsh translation is avaliable

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -15,9 +15,10 @@ private object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-25" % "4.12.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.35.0-play-25",
+    "uk.gov.hmrc" %% "govuk-template" % "5.40.0-play-25",
     "uk.gov.hmrc" %% "play-ui" % "7.40.0-play-25",
-    "uk.gov.hmrc" %% "crypto" % "5.4.0"
+    "uk.gov.hmrc" %% "crypto" % "5.4.0",
+    "uk.gov.hmrc" %% "play-language"      % "4.1.0"
   )
 
   abstract class TestDependencies(scope: String)(scopeOnlyDependencies: ModuleID*) {
