be996d7bcfe7dc7304b154f5ad5402489905d144
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-08-26 09:24:05 +0100

    [CIR-2287] Some depdendency bumps, no changes needed around assets. Removed sassify as no sass pipeline exists (#80)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,22 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.16.0"
-  private val playFrontendHmrcVersion = "12.7.0"
+  private val bootstrapVersion = "9.19.0"
+  private val playFrontendHmrcVersion = "12.8.0"
+  private val playSuffix = "-play-30"
 
   private val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % playFrontendHmrcVersion
+    "uk.gov.hmrc" %% s"bootstrap-frontend$playSuffix" % bootstrapVersion,
+    "uk.gov.hmrc" %% s"play-frontend-hmrc$playSuffix" % playFrontendHmrcVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"          % bootstrapVersion % Test,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"            % "2.19.1"         % Test,
-    "org.scalacheck"               %% "scalacheck"                      % "1.18.1"         % Test,
-    "org.mockito"                  %% "mockito-scala-scalatest"         % "2.0.0"          % Test
-  )
+    "uk.gov.hmrc"                  %% s"bootstrap-test$playSuffix"      % bootstrapVersion,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"            % "2.19.2",
+    "org.scalacheck"               %% "scalacheck"                      % "1.18.1",
+    "org.mockito"                  %% "mockito-scala-scalatest"         % "2.0.0"
+  ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
 }
