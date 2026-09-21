32471056928be90f6560ab42aec1867333d8098c
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2026-09-21 09:30:25 +0100

    CIR-3212 Update non play-frontend-hmrc dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
   private val playFrontendHmrcVersion = "12.32.0"
   private val scalatestplusVersion    = "3.2.19.0"
   private val playSuffix = "-play-30"
